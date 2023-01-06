7a54e69efaba5d237349eb9fdf9030c0d39b8bef
enyichiaagu <enyichiaagu@gmail.com>
2023-01-06 20:55:24 +0100

    Implemented active link styling

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,10 +1,10 @@
-import { Form, Link, Outlet, useLoaderData } from 'react-router-dom'
+import { Form, NavLink, Outlet, redirect, useLoaderData, useNavigation } from 'react-router-dom'
 
 import { getContacts, createContact } from '../contacts'
 
 export async function action() {
     const contact = await createContact()
-    return { contact }
+    return redirect(`/contacts/${contact.id}/edit`)
 }
 
 export async function loader() {
@@ -15,6 +15,7 @@ export async function loader() {
 export default function Root() {
 
     const { contacts } = useLoaderData()
+    let navigation = useNavigation()
 
     return (
       <>
@@ -48,7 +49,10 @@ export default function Root() {
                 <ul>
                 {contacts.map((contact) => (
                     <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink 
+                      to={`contacts/${contact.id}`}
+                      className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : "" }
+                    >
                         {contact.first || contact.last ? (
                         <>
                             {contact.first} {contact.last}
@@ -57,7 +61,7 @@ export default function Root() {
                         <i>No Name</i>
                         )}{" "}
                         {contact.favorite && <span>★</span>}
-                    </Link>
+                    </NavLink>
                     </li>
                 ))}
                 </ul>
@@ -68,7 +72,7 @@ export default function Root() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div id="detail" className={ navigation.state === "loading" ? "loading" : "" }>
             <Outlet />
         </div>
       </>
