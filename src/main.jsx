23dbd16ac01480cb4ff684b97244326108dcd0bc
enyichiaagu <enyichiaagu@gmail.com>
2023-01-06 08:08:56 +0100

    minimal set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Root, { loader as rootLoader } from './routes/root'
+import ErrorPage from './error-page'
+import Contact from './routes/contact'
+
 import './index.css'
+
+const router = createBrowserRouter([
+  { 
+    path: '/', 
+    element: <Root />, 
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: '/contacts/:contactId',
+        element: <Contact />,
+      }
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
