12ca7f1c400cd5dfc88d4d790c6f12a70b9cceef
enyichiaagu <enyichiaagu@gmail.com>
2023-01-06 21:26:06 +0100

    Added functionality for cancel button while editing contact

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import Root, { loader as rootLoader, action as rootAction } from './routes/root'
+import Index from './routes'
 import Contact, { loader as contactLoader } from './routes/contact'
 import EditContact, { action as editAction } from './routes/edit'
+import { action as destroyAction } from './routes/destroy'
 import ErrorPage from './error-page'
 
 import './index.css'
@@ -19,6 +21,10 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [ 
       {
+        index: true,
+        element: <Index />,
+      },
+      {
         path: 'contacts/:contactId',
         element: <Contact />,
         loader: contactLoader
@@ -28,6 +34,11 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction
+      },
+      {
+        path: 'contacts/:contactId/destroy',
+        action: destroyAction,
+        errorElement: <div>Oops. There was an error</div>
       }
     ]
   },
