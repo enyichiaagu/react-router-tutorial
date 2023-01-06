9eb3ce8071af06f1bcf823e28127a5e6e423dd45
enyichiaagu <enyichiaagu@gmail.com>
2023-01-06 16:34:36 +0100

    Implemented functionality for updating contacts with form data

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import Root, { loader as rootLoader, action as rootAction } from './routes/root'
-import Contact from './routes/contact'
+import Contact, { loader as contactLoader } from './routes/contact'
+import EditContact, { action as editAction } from './routes/edit'
 import ErrorPage from './error-page'
 
 import './index.css'
@@ -18,8 +19,15 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [ 
       {
-        path: '/contacts/:contactId',
+        path: 'contacts/:contactId',
         element: <Contact />,
+        loader: contactLoader
+      },
+      {
+        path: 'contacts/:contactId/edit',
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction
       }
     ]
   },
